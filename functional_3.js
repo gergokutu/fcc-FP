218571be3432c13210b11ddc457cc1c9b5a2b880
gergokutu <gergo.kutu@gmail.com>
2020-11-25 16:33:25 +0100

    Pass the value as a parameter to incrementer

--- functional_3.js
+++ functional_3.js
@@ -4,3 +4,8 @@ var fixedValue = 4;
 const incrementer = () => fixedValue + 1;
 console.log('Should be 5 >', incrementer());
 console.log('fixedValue should be 4 >', fixedValue);
+
+// Pass Arguments to Avoid External Dependence in a Function
+const incrementer2 = (value) => value + 1;
+console.log('Should be 5 >', incrementer2(fixedValue));
+console.log('fixedValue should be 4 >', fixedValue);
