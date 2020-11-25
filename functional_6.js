4f33a7789ad2a02e122c248811d7ea8075078d55
gergokutu <gergo.kutu@gmail.com>
2020-11-25 18:18:39 +0100

    Implement your own map() method on Array.prototype

---
+++ functional_6.js
@@ -0,0 +1,43 @@
+// Implement your own map() on a prototype
+// Do not forget the THIS!!!
+const s = [23, 65, 98, 5];
+
+// Array.prototype.myMap = function (callback) {
+//   var newArray = [];
+
+//   // Ver1
+//   for (let i = 0; i < this.length; i++) {
+//     newArray[i] = callback(this[i]);
+//   };
+
+//   // Ver2
+//   // for (let i = 0; i < this.length; i++) {
+//   //   newArray.push(callback(this[i]));
+//   // };
+
+//   // Ver3
+//   // this.forEach(num => newArray.push(callback(num)));
+
+//   return newArray;
+// };
+
+// Ver4 - recursive
+// Array.prototype.myMap = function (callback, arr = [], i = 0) {
+//   return i >= this.length
+//     ? arr
+//     : this.myMap(callback, arr.concat(callback(this[i])), i + 1);
+// };
+
+// Ver4 - recursive
+// Do not forget the THIS > this.length, this[i]
+// THIS refers to the Array on which the myMap is called!!!
+Array.prototype.myMap = function (callback, arr = [], i = 0) {
+  return i < this.length
+    ? this.myMap(callback, arr.concat(callback(this[i])), i + 1)
+    : arr;
+};
+
+const new_s = s.myMap(function (item) {
+  return item * 2;
+});
+console.log('Should be true [46, 130, 196, 10] >', new_s);
