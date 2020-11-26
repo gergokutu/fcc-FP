7195169f0bfe7c750644c60ded6c07f1532582a7
gergokutu <gergo.kutu@gmail.com>
2020-11-26 14:52:41 +0100

    Remove elements from an array with slice instead of splice

--- functional_09.js
+++ functional_09.js
@@ -2,6 +2,12 @@
 const sliceArray = (animals, beginSlice, endSlice) => (
   animals.slice(beginSlice, endSlice)
 );
-
 const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
 console.log("Should be [ Dog, Tiger ] >", sliceArray(inputAnim, 1, 3));
+
+// Remove Elements from an Array Using slice Instead of splice
+const nonMutatingSplice = cities => cities.slice(0, 3);
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+console.log("Should be [ Chicago, Delhi, Islamabad ] >", nonMutatingSplice(inputCities));
+
+// Combine Two Arrays Using the concat Method
