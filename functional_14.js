9aedaf8f8b9e446357c7297638ebfd431f2a5ae5
gergokutu <gergo.kutu@gmail.com>
2020-11-27 16:36:46 +0100

    Implement curried add()

--- functional_14.js
+++ functional_14.js
@@ -33,3 +33,17 @@ function impartial(x, y, z) {
 };
 const partialFn = impartial.bind(this, 1, 2);
 console.log('Should be 13 >', partialFn(10)); // Returns 13
+
+// Implement add
+// My 1st
+// function add(x) {
+//   return function (y) {
+//     return function (z) {
+//       return x + y + z;
+//     };
+//   };
+// };
+
+// My 2nd
+const add = x => y => z => x + y + z;
+console.log('Should be 60 >', add(10)(20)(30));
