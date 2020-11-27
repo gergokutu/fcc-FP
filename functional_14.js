de42f6e1db012ba4cb3fc62829ccdbd835dc88ae
gergokutu <gergo.kutu@gmail.com>
2020-11-27 15:49:46 +0100

    Implement anyPositive

--- functional_14.js
+++ functional_14.js
@@ -1,3 +1,7 @@
 // Use the every Method to Check that Every Element in an Array Meets a Criteria
 const checkPositive = arr => arr.every(num => num > 0);
 console.log('Should be false >', checkPositive([1, 2, 3, -4, 5]));
+
+// Use the some Method to Check that Any Elements in an Array Meet a Criteria
+const anyPositive = arr => arr.some(num => num > 0);
+console.log('Should be true >', anyPositive([1, 2, 3, -4, 5]));
