40c99d3406fd84c8327b4ad8292dfd271703b4fc
gergokutu <gergo.kutu@gmail.com>
2020-11-27 11:37:33 +0100

    Implement alphabeticalOrder

---
+++ functional_12.js
@@ -0,0 +1,12 @@
+// Sort an Array Alphabetically using the sort Method
+// My 1st
+const alphabeticalOrder = arr => {
+  return arr.sort((a, b) => {
+    return a === b
+      ? 0
+      : a < b
+        ? -1
+        : 1;
+  })
+};
+console.log('Test >', alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
